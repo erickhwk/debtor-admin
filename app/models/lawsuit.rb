d61f3275f2a16f39f958adbfe070eaef832d160f
Erick Valente <erickhwk@gmail.com>
2024-07-13 20:25:58 -0300

    bugfix: the relation between company and lawsuit is destroyed properly if the lawsuit or the company is deleted

--- app/models/lawsuit.rb
+++ app/models/lawsuit.rb
@@ -2,7 +2,8 @@ class Lawsuit < ApplicationRecord
   belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id', required: true
   belongs_to :tenancy
   has_many :reports, dependent: :destroy
-  has_and_belongs_to_many :companies
+  has_many :company_lawsuits, dependent: :destroy
+  has_many :companies, through: :company_lawsuits
 
   enum category: { bankruptcy: 0, judicial_recovery: 1 }
 

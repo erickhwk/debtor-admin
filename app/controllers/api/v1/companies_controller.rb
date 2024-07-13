17be7ce5413e17f087faebb2aded6409bd038145
Erick Valente <erickhwk@gmail.com>
2024-07-13 08:10:09 -0300

    fixed companies api controller to query with lawsuits

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::CompaniesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if params[:tenancy_id].present?
-      @companies = Company.joins(:lawsuits).where(lawsuits: {tenancy_id: params[:tenancy_id] }).reverse
+    if params[:lawsuit_id].present?
+      @companies = Company.joins(:lawsuits).where(lawsuits: {id: params[:lawsuit_id]}).reverse
       render json: @companies
     else
-      render json: { error: 'Tenancy ID is required' }, status: :unprocessable_entity
+      render json: { error: 'Lawsuit ID is required' }, status: :unprocessable_entity
     end
   end
 end

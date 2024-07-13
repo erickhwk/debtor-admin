44eb7df23173c8d5c4bdb64e2d4847fb69561e63
Erick Valente <erickhwk@gmail.com>
2024-07-12 22:48:24 -0300

    fixed reports api to fetch by lawsuits

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::ReportsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if params[:tenancy_id].present?
-      @reports = Report.joins(:lawsuit).where(lawsuits: { tenancy_id: params[:tenancy_id] }).reverse
+    if params[:lawsuit_id].present?
+      @reports = Report.where(lawsuit_id: params[:lawsuit_id] ).reverse
       render json: @reports, each_serializer: ReportSerializer
     else
-      render json: { error: 'Tenancy ID is required' }, status: :unprocessable_entity
+      render json: { error: 'Lawsuit ID is required' }, status: :unprocessable_entity
     end
   end
 end
